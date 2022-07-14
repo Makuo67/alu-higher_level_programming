c4290f58c00e79e66a5179c63e9ee6d630e1a875
Makuo67 <okekemakuo67@gmail.com>
2022-07-14 15:41:24 -0700

    remove an element of a list

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in range(str(my_string)):
+    for i in range(len(my_string)):
         my_string = my_string.remove('c', 'C')
     print(my_string)
