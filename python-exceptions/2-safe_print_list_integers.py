2412ef88eca588dff520128f7fe5ff75d06d28b0
Makuo67 <okekemakuo67@gmail.com>
2022-07-20 15:09:52 -0700

    Executable file

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,11 +3,10 @@ def safe_print_list_integers(my_list=[], x=0):
     funct = 0
     for i in range(x):
         try:
-            my_list.append(i)
-            if i == type(str):
-                my_list.remove(i)
-            funct += 1
+            print("{}".format(my_list[i]), end="")
         except (ValueError, TypeError):
-            break
-    print("{:d}".format(my_list[0]), end="")
-
+            pass
+        else:
+            funct += 1
+    print()
+    return (funct)
