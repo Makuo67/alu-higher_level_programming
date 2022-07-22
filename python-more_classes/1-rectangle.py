9bef3f0a65f3957eb0ee272def9166ffd603ac90
Makuo67 <okekemakuo67@gmail.com>
2022-07-22 13:34:17 -0700

    Executable file

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,7 +5,6 @@
 
 class Rectangle:
     """Write a class Rectangle that defines a rectangle"""
-
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
