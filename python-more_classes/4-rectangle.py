cc80652c1886e82169262bc8dd143e3d96b21e45
Okeke Makuochukwu <99495812+Makuo67@users.noreply.github.com>
2022-07-22 23:30:15 +0200

    Update 4-rectangle.py

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -66,5 +66,6 @@ class Rectangle:
 
     def __repr__(self):
         """Return the string representation of the Rectangle."""
-        rect = "Rectangle" + "(" + str(self.__width)
+        rect = "Rectangle(" + str(self.__width)
         rect += ", " + str(self.__height) + ")"
+        return (rect)
