256461f8998c0bb4e56cdf4ec7180d16570d6bbd
Okeke Makuochukwu <99495812+Makuo67@users.noreply.github.com>
2022-08-05 14:50:41 +0200

    Update 12-no_cheating.sql

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,5 +1,6 @@
 -- A script that updates the score of Bob to 10 in the table second_table
 -- Query to update the score of BOB to 10
-UPDATE FROM second_table
+UPDATE second_table
 SET score = 10
-WHERE name = 'Bob';
+WHERE name='Bob';
+
