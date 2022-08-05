2bfd5d3ac98e6a9be4252d41af1bc9bb94648c8a
Makuo67 <okekemakuo67@gmail.com>
2022-08-05 05:10:14 -0700

    Query file

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,9 @@
+-- A script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
+-- Query to create a second table
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT,
+	name VARCHAR(26),
+	score INT);
+INSERT INTO id AUTO_INCREMENT;
+INSERT INTO name VALUES ('John', 'Alex', 'Bob', 'George');
+INSERT INTO score VALUES (10, 3, 14, 8);
