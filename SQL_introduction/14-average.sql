f81c64714884b5ba382acb84cd920d5000f57e2b
Makuo67 <okekemakuo67@gmail.com>
2022-08-05 06:07:11 -0700

    Query file

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,5 +1,3 @@
 -- A script that computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.
 -- Query to compute the average score of all records in the table
-UPDATE second_table
-ADD average
-SET average = AVG(score);
+SELECT AVG(score) AS average FROM second_table;
