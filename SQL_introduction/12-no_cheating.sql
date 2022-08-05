f007998911e9a06564e8bf87e1d079f8b079baa8
Makuo67 <okekemakuo67@gmail.com>
2022-08-05 05:46:16 -0700

    Query fle

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,5 +1,5 @@
 -- A script that updates the score of Bob to 10 in the table second_table
 -- Query to update the score of BOB to 10
-SELECT score, name FROM second_table
+UPDATE FROM second_table
 WHERE name='Bob'
-UPDATE score = 10;
+score = 10;
