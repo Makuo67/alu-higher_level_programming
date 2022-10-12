e07606d74a6359486d9a88d0fe10439934441309
Makuo67 <okekemakuo67@gmail.com>
2022-10-12 08:11:43 -0700

    script file

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// A script that computes and prints a factorial
+const args = process.argv.slice(2);
+function factorial (n) {
+  if (n < 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const n = parseInt(args[0]);
+if (isNaN(n)) {
+  console.log(1);
+} else {
+  console.log(factorial(n));
+}
