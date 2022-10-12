ae2944bf226aca5b149f888ca156e4a9ee090318
Makuo67 <okekemakuo67@gmail.com>
2022-10-12 10:52:50 -0700

    advanced script file

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
 function callMeMoby (x, theFunction) {
   for (let i = 0; i < x; i++) {
-	theFunction();
+    theFunction();
   }
 }
 module.exports = { callMeMoby };
