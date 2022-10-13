2a40b5732fbe32a7f1a2ba57007643f1f54230c8
Okeke Makuochukwu <99495812+Makuo67@users.noreply.github.com>
2022-10-13 21:40:22 +0200

    Create 6-square.js

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const parentSquare = require('./5-square');
+
+class Square extends parentSquare {
+  charPrint (c) {
+    if (c !== undefined) {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    } else {
+      this.print();
+    }
+  }
+}
+module.exports = Square;
