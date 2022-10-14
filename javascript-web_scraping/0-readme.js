35807c98d0ee60b91298314eeaac04843de730b8
Okeke Makuochukwu <99495812+Makuo67@users.noreply.github.com>
2022-10-14 21:19:10 +0200

    Update 0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
-const file = process.argv.slice(2)[0];
 const fs = require('fs');
 
-fs.readFile(file, 'utf8', (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  }
+fs.readFile(process.argv[2], (err, contents) => {
+  err ? console.log(err) : console.log(contents.toString());
 });
